eaaf93215373b3590387bad896199d6c8c9ea78d
ia <isaac.ardis@gmail.com>
2023-12-07 10:58:11 -0700

    add pwd to workdir, dont have to rebuild image

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 set -x
-docker build -t geocat  .
+
+#docker build -t geocat  .
+
+mkdir -p $(pwd)/output
+
 docker run -i -t \
   --mount type=bind,source=$HOME/tdata,target=/tdata \
   --mount type=bind,source=$(pwd)/output,target=/tmp/output \
+  --mount type=bind,source=$(pwd),target=/app \
   --memory 16GB \
   --memory-swap 32GB \
   geocat
